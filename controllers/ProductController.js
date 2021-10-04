449beb81d17465315afa3e8ef1996253ebb2816b
DerickCSilva <derick.cardoso17@gmail.com>
2021-10-04 15:24:53 -0300

    fix: formatando retorno para moeda

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -168,8 +168,13 @@ class ProductController {
                 err
             });
         }
-        
+
         let product = await Product.findOne({ where: { id } });
+
+        let unitPriceFormatted = formatMoney(product.unitPrice);
+        let costPriceFormatted = formatMoney(product.costPrice);
+        product.unitPrice = unitPriceFormatted;
+        product.costPrice = costPriceFormatted;
 
         if (product) {
             return res.json({
