a5316b8d8fbc973de5ae728d4ae06c67026cf5ca
DerickCSilva <derick.cardoso17@gmail.com>
2021-10-05 09:52:20 -0300

    feat: tirando a pasta public do projeto (inutilizada)

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'mysql',
-    timezone: '-03:00'    
+    timezone: '-03:00'
 });
 
 module.exports = connection;
